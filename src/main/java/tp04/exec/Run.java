7f1c62344f1d738e1d6766592fd98aecc2b0cfd5
GeraldZHAO <98470468+GeraldZHAO@users.noreply.github.com>
2023-03-07 11:26:21 +0100

    Update Run.java

--- src/main/java/tp04/exec/Run.java
+++ src/main/java/tp04/exec/Run.java
@@ -16,7 +16,7 @@ import tp04.metier.Jour;
 import tp04.metier.Portefeuille;
 
 public class Run {
-
+    //Commentaire inutile.
     public static void main(String[] args) {
         ActionSimple bnp, axa;
         ActionComposee bqAss;
