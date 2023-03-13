1cefb61992e838747ea8a70bc965288611e7c206
Ryan <Ryan@LAPTOP-TDD4U2BJ>
2023-03-13 11:09:29 +0100

    us9 l'evolution d'une action simple

--- src/main/java/tp04/metier/ActionSimple.java
+++ src/main/java/tp04/metier/ActionSimple.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package tp04.metier;
 
 import java.util.ArrayList;
@@ -16,25 +15,27 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 /**
- * 
+ *
  * @author perussel
  */
-public class ActionSimple extends Action {    
+public class ActionSimple extends Action {
+
     // attribut lien
     private Map<Jour, Cours> mapCours;
-    
+
     /**
      * Constructeur d'une action simple
+     *
      * @param libelle
-     * 
+     *
      */
     public ActionSimple(String libelle) {
         // Action simple initialisée comme 1 action
         super(libelle);
         // init spécifique
-        this.mapCours = new HashMap();     
+        this.mapCours = new HashMap();
     }
-    
+
     //Getters 
     public Map<Jour, Cours> getMapCours() {
         return mapCours;
@@ -44,84 +45,115 @@ public class ActionSimple extends Action {
     public void setMapCours(Map<Jour, Cours> mapCours) {
         this.mapCours = mapCours;
     }
+
     /**
      * Obtenir le cours d'une action simple par un jour donnee
+     *
      * @param j
      * @return float cours
      */
-   
-    public float getCours(Jour j){
+
+    public float getCours(Jour j) {
         return getMapCours().get(j).getValeur();
     }
-    
+
     /**
      * Obtenir le cours d'une action simple le plus haut par un jour donnee
+     *
      * @param j
-     * @return 
+     * @return
      */
-        public float getCoursPlusHaut(){
+    public float getCoursPlusHaut() {
 
-            float max = 0f;
-            Map<Jour, Cours> map=getMapCours();
-            
-            float value = 0f;
-            
-            Set<Entry<Jour, Cours>> entryset=map.entrySet();
-            
-            for(Entry<Jour, Cours> entry : entryset){
-            
-                value= entry.getValue().getValeur();
-                
-                if(max<value){
-                    max=value;
-                }
+        float max = 0f;
+        Map<Jour, Cours> map = getMapCours();
+
+        float value = 0f;
+
+        Set<Entry<Jour, Cours>> entryset = map.entrySet();
+
+        for (Entry<Jour, Cours> entry : entryset) {
+
+            value = entry.getValue().getValeur();
+
+            if (max < value) {
+                max = value;
             }
-        return max;
         }
-    
-        
+        return max;
+    }
+
     /**
      * Obtenir le cours d'une action simple le plus bas par un jour donnee
+     *
      * @param j
-     * @return 
+     * @return
      */
-        public float getCoursPlusBas(){
-          float min = 1000000f;
-            Map<Jour, Cours> map=getMapCours();
-            
-            float value = 0f;
-            
-            Set<Entry<Jour, Cours>> entryset=map.entrySet();
-            
-            for(Entry<Jour, Cours> entry : entryset){
-            
-                value= entry.getValue().getValeur();
-                
-                if(min>value){
-                    min=value;
-                }
+    public float getCoursPlusBas() {
+        float min = 1000000f;
+        Map<Jour, Cours> map = getMapCours();
+
+        float value = 0f;
+
+        Set<Entry<Jour, Cours>> entryset = map.entrySet();
+
+        for (Entry<Jour, Cours> entry : entryset) {
+
+            value = entry.getValue().getValeur();
+
+            if (min > value) {
+                min = value;
             }
+        }
         return min;
     }
-    
-    
+
+    public Map<Jour, Cours> getEvolution(Jour j1, Jour j2) {
+
+        Map<Jour, Cours> map = getMapCours();
+        Map<Jour, Cours> mapResultat = new HashMap<>();
+
+        Set<Entry<Jour, Cours>> entryset = map.entrySet();
+        int anneeDebut = j1.getAnnee();
+        int anneeFin = j2.getAnnee();
+        int noJourDebut = j1.getNoJour();
+        int noJourFin = j2.getNoJour();
+        
+        int annee;
+        int nojour;
+        
+        for (Entry<Jour, Cours> entry : entryset) {
+
+            annee = entry.getValue().getJour().getAnnee();
+            nojour = entry.getValue().getJour().getNoJour();
+            if (anneeDebut >= annee && annee <= anneeFin) {
+                if (noJourDebut >= nojour && nojour <= noJourFin) {
+                    mapResultat.put(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+        return mapResultat;
+    }
+
     // enrg possible si pas de cours pour ce jour
     public void enrgCours(Jour j, float v) {
-        if(this.mapCours.containsKey(j) == false)
+        if (this.mapCours.containsKey(j) == false) {
             this.mapCours.put(j, new Cours(j, v));
+        }
     }
-    
+
     @Override
     public float valeur(Jour j) {
-        if(this.mapCours.containsKey(j) == true)
+        if (this.mapCours.containsKey(j) == true) {
             return this.mapCours.get(j).getValeur();
-        else 
+        } else {
             return 0; // definition d'une constante possible
+        }
     }
-  
+
     // encapsulation de la définition de la classe Cours
-    private class Cours {
-        
+    public static class Cours {
+
         private Jour jour;
 
         private float valeur;
@@ -129,7 +161,7 @@ public class ActionSimple extends Action {
         public float getValeur() {
             return valeur;
         }
-        
+
         public Jour getJour() {
             return jour;
         }
--- src/test/java/tp04/metier/ActionSimpleTest.java
+++ src/test/java/tp04/metier/ActionSimpleTest.java
@@ -4,8 +4,11 @@
  */
 package tp04.metier;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tp04.metier.ActionSimple.Cours;
 
 /**
  *
@@ -69,5 +72,36 @@ public class ActionSimpleTest {
         final float rs = as.getCoursPlusBas();
         
         Assertions.assertEquals(5.90f, rs,"La label de l'action doit etre celui fournit au constrcteur");
+    }
+    public void testEvolutionAction() {
+        final ActionSimple as = new ActionSimple("A1");   
+        final Jour jourDebut = new Jour(2023,101); 
+        final Jour jourFin = new Jour(2022,103); 
+        
+        final Jour j1 = new Jour(2022,100); 
+        final Jour j2 = new Jour(2022,108); 
+        final Jour j3 = new Jour(2023,100);
+        final Jour j4 = new Jour(2022,101); 
+        final Jour j5 = new Jour(2023,102); 
+        final Jour j6 = new Jour(2023,103);
+        final Jour j7 = new Jour(2023,104);
+        final Jour j8 = new Jour(2024,104);
+     
+        as.enrgCours(j1, 6.45f);
+        as.enrgCours(j2, 7.00f);
+        as.enrgCours(j3, 6.34f);
+        as.enrgCours(j4, 7.14f);
+        as.enrgCours(j5, 6.99f);
+        as.enrgCours(j6, 7.12f);
+        as.enrgCours(j7, 6.64f);
+        as.enrgCours(j8, 7.70f);
+        
+        final Map<Jour, ActionSimple.Cours> m  = as.getEvolution(jourDebut, jourFin);
+        final Map<Jour, ActionSimple.Cours> mTest=new HashMap<>();
+        mTest.put(j4, new Cours(j4,7.14f));
+        mTest.put(j5, new Cours(j5,6.99f));
+        mTest.put(j6, new Cours(j6,7.12f));
+        Assertions.assertNotNull(m,"Le map n'est pas NULL"); 
+        Assertions.assertEquals(mTest, m,"Equal");
     }
 }
